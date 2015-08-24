f23deaf99f31bf99662a4a28af16140c1686068e
Christian de la Cruz-Munoz <cdelacruzmunoz@palmertrinity.org>
2015-08-24 14:51:16 -0400

    says hello world

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class helloWorld here.
+ * 
+ * @author Christian de la Cruz-Munoz
+ * @version (2015-08-24)
+ */
+public class HelloWorld
+{
+
+    public static void main(String [] args)
+    {
+        System.out.print("Hello World!");
+    }
+
+}
